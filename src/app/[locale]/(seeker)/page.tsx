8efef8a0a490c900d710239cebc229226353744a
Respectus11 <kalexkalab@gmail.com>
2026-09-23 14:30:00 +0300

    feat: wire up Seeker Landing view

--- src/app/[locale]/(seeker)/page.tsx
+++ src/app/[locale]/(seeker)/page.tsx
@@ -1,12 +1,23 @@
-import { setRequestLocale } from "next-intl/server";
-import { SeekerComposer } from "@/components/seeker/SeekerComposer";
+import React from "react";
+import { SeekerHeader } from "@/components/seeker/SeekerHeader";
+import { SeekerHero } from "@/components/seeker/SeekerHero";
+import { ReflectionComposer } from "@/components/seeker/ReflectionComposer";
+import { CategoryPillsGrid } from "@/components/seeker/CategoryPillsGrid";
+import { SeekerBottomNav } from "@/components/seeker/SeekerBottomNav";
 
-export default async function LandingPage({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}) {
-  const { locale } = await params;
-  setRequestLocale(locale);
-  return <SeekerComposer />;
+export default function SeekerLandingPage() {
+  return (
+    <div className="bg-canvas-deep text-on-surface font-body-md min-h-screen antialiased selection:bg-primary-container selection:text-on-primary-container relative pb-52">
+      <SeekerHeader />
+
+      {/* Main Scrollable Canvas */}
+      <main className="w-full max-w-md mx-auto pt-16 px-margin flex flex-col gap-6">
+        <SeekerHero />
+        <ReflectionComposer />
+        <CategoryPillsGrid />
+      </main>
+
+      <SeekerBottomNav />
+    </div>
+  );
 }
