60a963198e3f919c620f9b616f9733a04008f7c0
Respectus11 <kalexkalab@gmail.com>
2026-09-24 14:01:23 +0300

    fix: seeker thread page reads code from searchParams, redirects if missing

--- src/app/[locale]/(seeker)/thread/page.tsx
+++ src/app/[locale]/(seeker)/thread/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { redirect } from "next/navigation";
 import { SeekerThreadView } from "@/components/seeker/SeekerThreadView";
 
-export default function SeekerThreadPage() {
-  return <SeekerThreadView />;
+export default async function SeekerThreadPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ code?: string }>;
+}) {
+  const { code } = await searchParams;
+  if (!code) redirect("/");
+  return <SeekerThreadView code={code} />;
 }
